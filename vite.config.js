8466c7a18ad989f7fe1de9e681e9a299b1aca092
rraj285181 <rraj285181@example.com>
2026-07-15 10:01:35 +0530

    fix: remove tutorials.html entry from vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         compiler: resolve(__dirname, 'compiler.html'),
-        tutorials: resolve(__dirname, 'tutorials.html'),
         python: resolve(__dirname, 'python-tutorial.html'),
         cpp: resolve(__dirname, 'cpp-tutorial.html'),
         java: resolve(__dirname, 'java-tutorial.html')
